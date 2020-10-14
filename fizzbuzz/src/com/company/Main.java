e74df064f1e6d3e090aa7689bf3d2e4e6436a86f
mmyotane <miika.myotanen@hotmail.com>
2020-10-14 17:44:13 +0300

    fizz

---
+++ fizzbuzz/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    fizzBuzz();
+    }
+
+    public static void fizzBuzz() {
+        for (int i=1; i < 101; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
