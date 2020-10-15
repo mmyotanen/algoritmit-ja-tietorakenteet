6049f6f486fdce710303b083d0e82185a0f602f5
mmyotane <miika.myotanen@hotmail.com>
2020-10-15 17:23:03 +0300

    anagrammi

--- algoritmitTietorakenteet/src/com/company/Anagrammi.java
+++ algoritmitTietorakenteet/src/com/company/Anagrammi.java
@@ -1,4 +1,17 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Anagrammi {
+
+    public boolean Anagram(String text1, String text2) {
+        char[] chars1 = text1.toCharArray();
+        Arrays.sort(chars1);
+
+        char[] chars2 = text2.toCharArray();
+        Arrays.sort(chars2);
+
+
+        return Arrays.equals(chars1, chars2);
+    }
 }
