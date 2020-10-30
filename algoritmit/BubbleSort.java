b13195221e0e593eb6fb9f68367afa0db57eaaee
mmyotane <miika.myotanen@hotmail.com>
2020-10-30 16:47:16 +0200

    kommentti

--- algoritmit/BubbleSort.java
+++ algoritmit/BubbleSort.java
@@ -6,6 +6,7 @@ public class BubbleSort {
 
         for (int i = 0; i < n.length - 1; i++) {
             for (int j = 0; j < n.length - 1 - i; j++) {
+                //siirretään suurin viimeiseksi
                 if (n[j] > n[j+1]) {
                     int apu = n[j];
                     n[j] = n[j+1];
